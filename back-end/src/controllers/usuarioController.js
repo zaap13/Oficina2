196e7c7dd06472b8bf8d58fc83d7adb7b4d741f4
JorgeModos <135282850+JorgeModos@users.noreply.github.com>
2024-04-15 19:46:15 -0300

    Update "Cadastrar Usuario"

--- back-end/src/controllers/usuarioController.js
+++ back-end/src/controllers/usuarioController.js
@@ -2,11 +2,12 @@ import { criarNovoUsuario } from "../services/usuarioService.js";
 
 async function criarUsuario(req, res) {
   try {
-    const usuario = await criarNovoUsuario(req.body);
-    res.status(201).send(usuario);
+    const { nome, email } = req.body;
+    const senha = await criarNovoUsuario({nome, email});
+    res.status(201).json({ mensagem: "Usuário cadastrado com sucesso", senha });
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).json({ mensagem: error.message });
   }
-}
+  }
 
 export { criarUsuario };
