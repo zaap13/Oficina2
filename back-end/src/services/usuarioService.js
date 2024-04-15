7517bbf3e93f2290f20073c31bd5e0315cc61d50
JorgeModos <135282850+JorgeModos@users.noreply.github.com>
2024-04-15 19:48:39 -0300

    Update "Cadastrar Usuario"

--- back-end/src/services/usuarioService.js
+++ back-end/src/services/usuarioService.js
@@ -1,11 +1,9 @@
 import { criarUsuario } from "../repositories/usuarioRepository.js";
 
-async function criarNovoUsuario(data) {
-  try {
-    return await criarUsuario(data);
-  } catch (error) {
-    throw new Error("Erro ao criar usuario no banco de dados.");
-  }
+async function criarNovoUsuario(nome, email) {
+  const senha = Math.floor(1000 + Math.random() * 9000).toString();
+  await criarUsuario(nome, email, senha);
+  return senha;
 }
 
 export { criarNovoUsuario };
