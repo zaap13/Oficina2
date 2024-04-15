b2af2eb3c0368632a65656b19352e4ecbc71de5b
JorgeModos <135282850+JorgeModos@users.noreply.github.com>
2024-04-15 19:46:41 -0300

    Update "Cadastrar Usuario"

--- back-end/src/repositories/usuarioRepository.js
+++ back-end/src/repositories/usuarioRepository.js
@@ -1,7 +1,8 @@
 import Usuario from "../models/usuarioModel.js";
 
-async function criarUsuario(data) {
-  return await Usuario.create(data);
+async function criarUsuario(nome, email, senha) {
+  const usuario = new Usuario({ nome, email, senha });
+  return await usuario.save();
 }
 
 export { criarUsuario };
