ce4a281ab918e2623f4839e925457de8d80a2a31
JorgeModos <jorgemodos@alunos.utfpr.edu.br>
2024-04-19 17:59:06 -0300

    Teste (fixes #2)

---
+++ back-end/src/controllers/__tests__/usuarioController.test.js
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import app from '../app';
+
+describe('Teste do controlador de usuários', () => {
+  it('Deve criar um novo usuário', async () => {
+ 
+    const novoUsuario = {
+      nome: 'Alice',
+      email: 'alice@example.com',
+      tipo: 'coordenador'
+    };
+
+    // Faz uma solicitação POST para a rota /usuarios com os dados do novo usuário
+    const response = await request(app)
+      .post('/usuarios')
+      .send(novoUsuario);
+
+    // Verifica se a resposta possui o status HTTP 201 (Created)
+    expect(response.status).toBe(201);
+
+    // Verifica se a resposta possui uma mensagem de sucesso e uma senha
+    expect(response.body).toHaveProperty('mensagem', 'Usuário cadastrado com sucesso');
+    expect(response.body).toHaveProperty('senha');
+  });
+});
