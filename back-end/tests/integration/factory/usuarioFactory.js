715a2dbc6c78b249ec143889259a9d2d4fa6f8e7
JorgeModos <jorgemodos@alunos.utfpr.edu.br>
2024-05-03 21:20:44 -0300

    Update testes (fixes #6)

--- back-end/tests/integration/factory/usuarioFactory.js
+++ back-end/tests/integration/factory/usuarioFactory.js
@@ -6,3 +6,23 @@ factory.define('Usuario', {}, {
   email: faker.internet.email(),
   tipo: 'admin'
 });
+
+factory.define('UsuarioSemNome', {}, {
+  email: faker.internet.email(),
+  tipo: 'admin'
+});
+
+factory.define('UsuarioComEmailDuplicado', {}, async () => {
+  // Gera um e-mail aleatório
+  const email = faker.internet.email();
+  // Cria um usuário com o e-mail gerado para garantir a duplicação
+  await factory.create('Usuario', { email });
+  // Retorna um objeto com um e-mail duplicado
+  return { nome: faker.name.findName(), email, tipo: 'admin' };
+});
+
+factory.define('UsuarioComTipoInvalido', {}, {
+  nome: faker.name.findName(),
+  email: faker.internet.email(),
+  tipo: 'invalido'
+});
